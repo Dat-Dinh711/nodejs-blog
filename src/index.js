6694d1f202f8e3f0b3213e0d4d79f46a0313d507
DatDinh <datthanh071100@gmail.com>
2021-08-31 10:56:51 +0700

    Query parameters

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -19,11 +19,16 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+    // console.log(req.query.q);
+    res.render('search');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
